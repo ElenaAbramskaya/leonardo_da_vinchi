e63882a9771c19adf5e70e29548f1b312c8c8838
lenaab <68326730+lenaab@users.noreply.github.com>
2022-01-21 10:48:39 +0200

    fix syle.css

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const form = document.getElementsByClassName('form');
 let username = document.getElementById('author-name');
 let surname = document.getElementById('author-surname');
 let text = document.getElementById('text');
-
+let email = document.getElementById('author-email');
 let message = document.getElementById('message-1');
 let user = document.getElementById('author');
 let author_text = document.getElementById('author-text');
@@ -18,6 +18,9 @@ function showText() {
 
     user.innerText = `${username.value} ${surname.value}`;
     author_text.innerText = text.value;
-
+    username.value = "";
+    surname.value = "";
+    text.value = "";
+    email.value = "";
 };
 btn.addEventListener('click', showText);
