69d47f632f563b342556bae41debfdf81dc7499a
lenaab <68326730+lenaab@users.noreply.github.com>
2022-01-22 13:58:29 +0200

    fix description in main.js

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ function closePainting() {
     let back = document.createElement('img');
     back.src = "img/back.jpg";
     paint.appendChild(back);
-    back.style.marginTop = 0;
+    back.style.marginBottom = "20px";
+    back.style.marginRight = "20px";
     back.addEventListener('click', function() {
         gallery.hidden = false;
         paint.style.display = "none";
@@ -39,6 +40,7 @@ img.addEventListener('click', function() {
     img1.src = "img/gallery-img-2.jpg";
     paint.appendChild(img1);
 
+
     let text = document.createElement('p');
     paint.appendChild(text);
 
@@ -46,8 +48,7 @@ img.addEventListener('click', function() {
     paint.appendChild(description);
     text.innerText = "Богоматерь с младенцем";
 
-    description.innerHTML = "Принадлежит к позднему периоду его творчества. Выставлена в Большой галерее Лувра. Леонардо да Винчи использовал мало распространённый в Италии сюжет, известный как «Анна-втроём», когда Мария сидит на коленях у своей матери Анны и держит на руках младенца Иисуса.";
-
+    description.innerHTML = "Эта картина относится к позднему периоду творчества Леонардо. Ее дом сейчас находится в большой галерее Лувра. Да Винчи использовал непопулярный в то время сюжет для Италии под названием «Анна-трио». Святая Мария, сидящая на коленях у матери, держит на руках Младенца Иисуса. Леонардо сделал множество зарисовок к этой истории, но так и не завершил работу. Саму картину мастер писал приблизительно десять лет, но многие детали в ней так и остались незавершенными."
     paint.style.display = "flex";
     paint.style.flexDirection = "column";
     paint.style.alignItems = "center";
@@ -55,7 +56,12 @@ img.addEventListener('click', function() {
     img1.style.width = "400px";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
+
     description.style.width = "400px";
+    description.style.fontWeight = "bold";
+    description.style.color = "#7e8dc6";
+
 
 });
 second_img.addEventListener('click', function() {
@@ -75,10 +81,13 @@ second_img.addEventListener('click', function() {
     text.innerText = "Мадонна Литта";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Картина традиционно атрибутируемая как произведение выдающегося итальянского художника и учёного Высокого Возрождения Леонардо да Винчи, приблизительно датируемая периодом 1481—1495 годов. Хранится в собрании Эрмитажа в Санкт-Петербурге."
+    description.innerHTML = "Картина написанная талантливым да Винчи в начале 90-х 15-го столетия. Размер картины составляет 42х33 см. Материалы - дерево и темпера. «Мадонна Лита» ярко отображает элементы нового искусства эпохи Высокого Возрождения. Полотно хранится в галерее Эрмитажа в городе Санкт - Петербург."
     description.style.width = "400px";
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 
 });
 third_img.addEventListener('click', function() {
@@ -98,11 +107,13 @@ third_img.addEventListener('click', function() {
     text.innerText = "Мадонна в скалах";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Произведение выдающегося художника итальянского Возрождения Леонардо да Винчи, существующее, помимо множества подготовительных рисунков и этюдов, в двух основных вариантах. Одна картина (1483—1486) хранится в музее Лувр в Париже, вторая, написанная позднее, в период 1499—1506 годов, в — Лондонской национальной галерее.";
+    description.innerHTML = "Произведение выдающегося Леонардо, помимо многочисленных рисунков, существует в двух вариантах. Одна из картин (1483—1486) находится в музее Лувра в Париже. Второе полотно, которое написано позже, в период 1499—1506 годов, обитает в Лондонской галерее. Основой для композиции картины послужил малоизвестный сюжет из текста «Жизнь Иоанна по Серапиону».";
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 
 });
 fourth_img.addEventListener('click', function() {
@@ -121,12 +132,15 @@ fourth_img.addEventListener('click', function() {
     paint.appendChild(text);
     text.innerText = "Мадонна Бенуа или Мадонна с цветком";
     text.style.fontSize = "20px";
+    text.style.width = "220px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Ранняя картина Леонардо да Винчи, предположительно оставшаяся незавершённой. В 1914 году она была приобретена Императорским Эрмитажем у Марии Александровны, жены придворного архитектора Леонтия Николаевича Бенуа."
+    description.innerHTML = "Эта картина считается одной из первых и незавершенных работ Леонардо. В 1914 году ее приобрел в свою коллекцию Императорский Эрмитаж. Картину галерее продала жена придворного архитектора Бенуа - Мария Александровна. В работе над этим произведением, художником была использована техника масляной живописи."
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 fifth_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -145,11 +159,13 @@ fifth_img.addEventListener('click', function() {
     text.innerText = "Дама с горностаем";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Картина, как считается, принадлежащая кисти Леонардо да Винчи. По мнению многих исследователей, это портрет Чечилии Галлерани — любовницы Лодовико Сфорца по прозванию Иль Моро, герцога Миланского, что находит подтверждение в сложной символике картины."
+    description.innerHTML = "Картина была написана в 1489–1490 годы. Многие искусствоведы склонны утверждать, что на полотне изображена любовница Лодовико Сфорца. Женщину звали Чечилия Галлерани. Размеры картины 54 × 40 см. В настоящее время картина обитает в Национальном музее Кракова."
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 sixth_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -168,11 +184,13 @@ sixth_img.addEventListener('click', function() {
     text.innerText = "Мона Лиза";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Картина Леонардо да Винчи, одно из самых известных произведений живописи. Точная дата написания неизвестна (по некоторым сведениям, написана между 1503 и 1505 годами). Ныне хранится в Лувре. Считается, что на картине изображена Лиза Герардини, супруга флорентийского торговца шёлком Франческо дель Джокондо."
+    description.innerHTML = "Одна из самых известных и загадочных картин величайшего Леонардо да Винчи. Предположительно картина, написана в 1503 -1505 годы. Сейчас картина находиться в Лувре. По мнению искусствоведов, на картине мы видим жену продавца шёлка - Франческо дель Джокондо из Флоренции."
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 seventh_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -192,11 +210,13 @@ seventh_img.addEventListener('click', function() {
     text.innerText = "Портрет Джиневры де Бенчи";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Ранняя картина Леонардо да Винчи, написанная около 1474—1476 годов, образец флорентийской портретной живописи позднего кватроченто (эпохи Раннего Возрождения). Это женский портрет, на котором, как выяснили в XX веке, изображена Джиневра д’Америго де Бенчи, флорентийская поэтесса и интеллектуалка XV века, платоническая возлюбленная венецианца Бернардо Бембо, который, скорее всего, и заказал этот портрет художнику."
+    description.innerHTML = "Одна из ранних картин да Винчи. Картина была написана примерно в 1474—1476 годы. Полотно можно назвать примером флорентийской портретной живописи Раннего Возрождения. Это портрет женщины, флорентийской поэтессы 15 века - Джиневры д’Америго де Бенчи. Поэтесса была платонической любовью Бернардо Бембо, Он вероятнее и заказал этот портрет у мастера."
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 eigth_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -215,11 +235,13 @@ eigth_img.addEventListener('click', function() {
     text.innerText = "Иоанн Креститель";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
-    description.innerHTML = "Это произведение относится к позднему периоду творчества художника.лухой фон, лишенный пейзажа, столь характерного для работ эпохи Возрождения.Изображение святого снабжено традиционной атрибутикой: тонкий тростниковый крест, длинные волосы, одежды из шерсти. Пересечение диагоналей корпуса тела и правой руки усиливает мотив креста, который едва заметно написан художником."
+    description.innerHTML = "Картина относиться к познему этапу искуства Леонардо да Винчи. Полотно было написано в 1514-1516 годы. Подбор темного фона и композиции характеризуют произведение, как яркий пример живописи эпохи Возрождения. По центру мы видим портрет святого - Иоанна Крестителя."
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 ninth_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -238,11 +260,13 @@ ninth_img.addEventListener('click', function() {
     text.innerText = "Прекрасная Ферроньера";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
     description.innerHTML = "Портрет неизвестной миланской дамы (предположительно Лукреции Кривелли, фаворитки герцога Лодовико Моро, либо же его жены Беатриче д'Эсте), считающийся работой Леонардо да Винчи. Картина выставлена в Лувре.Около 1490 года сам Леонардо да Винчи — либо же его ученик Бернардино деи Конти (по предположению Беренсона); либо Больтраффио (по мнению Герберта Кука); а может Франческо Мельци — возможно, при участии учителя, написал данный портрет маслом на дереве. Иногда картину называют «Портретом незнакомки»"
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 tenth_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -261,11 +285,13 @@ tenth_img.addEventListener('click', function() {
     text.innerText = "Мадонна с гвоздикой";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
     description.innerHTML = "Картина, которую многие искусствоведы приписывают молодому Леонардо да Винчи. Предположительно, создана Леонардо в бытность его учеником в мастерской Верроккьо. Над сложным многообразием одеяния, которое цветовой гаммой сочетается с причудливой горной цепью на заднем плане, возвышается благородно поднятая глава Марии. Никакое чувство не проникает наружу, нарушая совершенные черты лица, только небольшое подобие улыбки играет на губах. В противоположность спокойной Мадонне, младенец Иисус изображен в энергичном движении. Он буквально дрожит от возбуждения, пытаясь схватить красную гвоздику, которую держит в своей грациозной руке его мать. "
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 eleventh_img.addEventListener('click', function() {
     gallery.hidden = true;
@@ -284,11 +310,13 @@ eleventh_img.addEventListener('click', function() {
     text.innerText = "Портрет музыканта";
     text.style.fontSize = "20px";
     text.style.fontWeight = "bold";
+    text.style.color = "#876d89";
     let description = document.createElement('p');
     paint.appendChild(description);
     description.innerHTML = "сильно переписанная в позднейшее время и неоконченная картина кисти великого итальянского художника Леонардо да Винчи, хранящаяся в миланской Пинакотеке Амброзиана. Картина начата художником на рубеже 90-х годов XV века. В XIX веке считалось, что портрет изображает миланского герцога Лодовико Моро."
     description.style.width = "400px";
-
+    description.style.color = "#7e8dc6";
+    description.style.fontWeight = "bold";
 });
 
 
